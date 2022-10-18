4da8322b21bc30fd51f74f73f5008b79d668baba
jigglypop <ydh2244@gmail.com>
2022-10-19 00:01:31 +0900

    1019

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -1,7 +1,7 @@
 echo "도커 빌드" >> /home/ubuntu/log.txt
 
-docker build -t jigglog-backend . >> /home/ubuntu/log.txt
-
-echo "도커 시작" >> /home/ubuntu/log.txt
-
-docker run -p 8080:8080 -t jigglog-backend >> /home/ubuntu/log.txt
+#docker build -t jigglog-backend . >> /home/ubuntu/log.txt
+#
+#echo "도커 시작" >> /home/ubuntu/log.txt
+#
+#docker run -p 8080:8080 -t jigglog-backend >> /home/ubuntu/log.txt
--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -1,3 +1,3 @@
 echo "이전 build 삭제" >> /home/ubuntu/log.txt
 
-rm -rf ./build >> /home/ubuntu/log.txt
+#rm -rf ./build >> /home/ubuntu/log.txt
