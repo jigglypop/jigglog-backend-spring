72684fdc92dba1dffdd161929f401716e2392327
jigglypop <ydh2244@gmail.com>
2022-10-20 23:25:53 +0900

    1020-9

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -2,6 +2,10 @@ echo "after deploy 시작" >> /home/ubuntu/log.txt
 
 cd /home/ubuntu/jigglogkotlin/ >> /home/ubuntu/log.txt
 
+SHELL_PATH=`pwd -P`
+
+echo "현재 경로 : " $SHELL_PATH >> /home/ubuntu/log.txt
+
 docker build -t jigglog-backend .
 
 echo "도커 빌드 완료" >> /home/ubuntu/log.txt
