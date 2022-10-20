a23478825cbde6e8920fe47a89441e09fb1e420a
jigglypop <ydh2244@gmail.com>
2022-10-21 00:05:24 +0900

    1021

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -40,7 +40,4 @@ JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
-nohup java -jar \
-        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/parrot/application-db.properties,/home/ec2-user/parrot/application-mail.properties,classpath:/application-real.properties \
-        -Dspring.profiles.active=real \
-        $REPOSITORY/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar $REPOSITORY/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -2,4 +2,8 @@ echo "before deploy 시작" >> /home/ubuntu/log.txt
 
 cd /home/ubuntu >> /home/ubuntu/log.txt
 
+rm -rf jigglogkotlin >> /home/ubuntu/log.txt
+
+echo "이전 폴더를 삭제" >> /home/ubuntu/log.txt
+
 echo "before deploy 완료" >> /home/ubuntu/log.txt
