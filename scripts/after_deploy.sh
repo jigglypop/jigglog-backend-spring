d482704a61fca34e43d3852ec855960f75aef0d9
jigglypop <ydh2244@gmail.com>
2024-03-31 02:48:35 +0900

    0331

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -1,32 +1,14 @@
-REPOSITORY=/home/ubuntu/jigglogkotlin
-PROJECT_NAME=jigglogkotlin
+HOME=/home/ubuntu
+REPOSITORY=jigglogkotlin
 
-echo "> Build 파일 복사" >> /home/ubuntu/log.txt
+now_time=`date`
+echo "[$now_time] --- 백엔드 배포 AFTER ---" >> /home/ubuntu/log.txt
+echo "[$now_time] --- 1) Build 파일 복사 ---" >> /home/ubuntu/log.txt
 
-cp $REPOSITORY/build/libs/*.jar $REPOSITORY/
+# 시작 로그 기록
+now_time=`date`
+echo "[$now_time] 2) after deploy 시작, 도커 컴포즈" >> /home/ubuntu/log.txt
 
-echo "> 현재 구동중인 애플리케이션 pid 확인" >> /home/ubuntu/log.txt
-
-CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
-
-echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID" >> /home/ubuntu/log.txt
-
-if [ -z "$CURRENT_PID" ]; then
-    echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/log.txt
-else
-    echo "> kill -9 $CURRENT_PID" >> /home/ubuntu/log.txt
-    kill -9 $CURRENT_PID
-    sleep 2
-fi
-
-echo "> 새 애플리케이션 배포" >> /home/ubuntu/log.txt
-
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
-
-echo "> JAR Name: $JAR_NAME" >> /home/ubuntu/log.txt
-
-#java -jar $REPOSITORY/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
-#nohup java -jar $REPOSITORY/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
-
-docker build -t jigglog-backend . &&
-docker compose -f docker-compose.yml up -d --build
+cd ${HOME}/${REPOSITORY} || exit >> /home/ubuntu/log.txt
+sudo docker build -t jigglogkotlin -f /home/ubuntu/jigglogkotlin/Dockerfile . &&
+sudo docker compose -f ${HOME}/${REPOSITORY} up -d --build
--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -1,12 +1,27 @@
-echo "before deploy 시작" >> /home/ubuntu/log.txt
+HOME=/home/ubuntu
+REPOSITORY=jigglogkotlin
 
-cd /home/ubuntu >> /home/ubuntu/log.txt
 
-rm -rf jigglogkotlin >> /home/ubuntu/log.txt
+now_time=`date`
+echo "[$now_time] --- 백엔드 배포 BEFORE ---" >> /home/ubuntu/log.txt
 
-echo "이전 폴더를 삭제" >> /home/ubuntu/log.txt
+# 1. 이전 폴더 삭제
+now_time=`date`
+echo "[$now_time] 1) 이전 폴더 확인 후 있으면 삭제" >> /home/ubuntu/log.txt
 
-docker rm -f $(docker ps -aq)
-docker rmi $(docker images -q)
+cd ${HOME} >> /home/ubuntu/log.txt
+# 폴더 여부 확인하고 있으면 삭제
+if [ -d ./$HOME ]; then
+    rm -rf ./${HOME} >> /home/ubuntu/log.txt
+    now_time=`date`
+    echo "[$now_time] 1-1) 이전 폴더 삭제" >> /home/ubuntu/log.txt
+fi
 
-echo "before deploy 완료 " >> /home/ubuntu/log.txt
+# 2. 이전 도커 컨테이너 삭제
+now_time=`date`
+echo "[$now_time] 2) 이전 도커 컨테이너 삭제" >> /home/ubuntu/log.txt
+docker rm -f ${REPOSITORY} >> /home/ubuntu/log.txt
+docker rmi -f ${REPOSITORY} >> /home/ubuntu/log.txt
+
+now_time=`date`
+echo "[$now_time] --- 백엔드 before deploy 완료 --- " >> /home/ubuntu/log.txt
