af378f33e696d061c1342a0178b6f44b561090b2
jigglypop <ydh2244@gmail.com>
2024-03-30 17:32:08 +0900

    0330

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -28,5 +28,5 @@ echo "> JAR Name: $JAR_NAME" >> /home/ubuntu/log.txt
 #java -jar $REPOSITORY/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 #nohup java -jar $REPOSITORY/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
-sudo docker build -t jigglog-backend . &&
+sudo docker build -t jigglogkotlin . &&
 sudo docker compose -f docker-compose.yml up -d --build
