994e61a90b56234012063c12a5eaf46d2456f79a
jigglypop <ydh2244@gmail.com>
2024-03-31 02:54:22 +0900

    0330

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 HOME=/home/ubuntu
 REPOSITORY=jigglogkotlin
 
@@ -12,24 +11,11 @@ echo "[$now_time] 1) 이전 폴더 확인 후 있으면 삭제" >> /home/ubuntu/
 
 cd ${HOME} >> /home/ubuntu/log.txt
 # 폴더 여부 확인하고 있으면 삭제
-if [ -d ./$HOME ]; then
-    rm -rf ./${HOME} >> /home/ubuntu/log.txt
+if [ -d ./$REPOSITORY ]; then
+    rm -rf ./${REPOSITORY} >> /home/ubuntu/log.txt
     now_time=`date`
     echo "[$now_time] 1-1) 이전 폴더 삭제" >> /home/ubuntu/log.txt
-=======
-PROJECT_NAME=jigglogkotlin-spring-boot
-
-echo "before deploy 시작" >> /home/ubuntu/log.txt
-
-cd /home/ubuntu >> /home/ubuntu/log.txt || exit
-
-# 폴더 여부 확인하고 있으면 삭제
-if [ -d ./jigglogkotlin ]; then
-  rm -rf ./jigglogkotlin >> /home/ubuntu/log.txt
-  echo "이전 폴더를 삭제" >> /home/ubuntu/log.txt
->>>>>>> 00db8e808b9e2674f47ff13a4ae45c7bd5193693
 fi
-
 # 2. 이전 도커 컨테이너 삭제
 now_time=`date`
 echo "[$now_time] 2) 이전 도커 컨테이너 삭제" >> /home/ubuntu/log.txt
