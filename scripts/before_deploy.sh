a19deae6e9945aa2cbfd6fe8258b439bbab27e04
jigglypop <ydh2244@gmail.com>
2024-03-30 17:21:52 +0900

    0330

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -2,13 +2,12 @@ PROJECT_NAME=jigglogkotlin-spring-boot
 
 echo "before deploy 시작" >> /home/ubuntu/log.txt
 
-cd /home/ubuntu >> /home/ubuntu/log.txt
+cd /home/ubuntu >> /home/ubuntu/log.txt || exit
 
-rm -rf jigglogkotlin >> /home/ubuntu/log.txt
-
-echo "이전 폴더를 삭제" >> /home/ubuntu/log.txt
-
-docker rm -f $PROJECT_NAME
-docker rmi $PROJECT_NAME
+# 폴더 여부 확인하고 있으면 삭제
+if [ -d ./jigglogkotlin ]; then
+  rm -rf ./jigglogkotlin >> /home/ubuntu/log.txt
+  echo "이전 폴더를 삭제" >> /home/ubuntu/log.txt
+fi
 
 echo "before deploy 완료 " >> /home/ubuntu/log.txt
